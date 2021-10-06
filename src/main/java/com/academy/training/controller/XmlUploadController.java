2aa3d76edc1173b1423b87c165a1f1fc95089ab1
AKOCAK <ayse.kocak@capgemini.com>
2021-10-06 14:12:33 +0200

    Initial commit

---
+++ src/main/java/com/academy/training/controller/XmlUploadController.java
@@ -0,0 +1,66 @@
+package com.academy.training.controller;
+
+import com.academy.training.generated.Aanvraag;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+import javax.xml.XMLConstants;
+import javax.xml.bind.JAXBContext;
+import javax.xml.bind.Unmarshaller;
+import javax.xml.stream.XMLInputFactory;
+import javax.xml.stream.XMLStreamReader;
+import javax.xml.validation.SchemaFactory;
+import java.io.InputStream;
+
+@RestController
+@RequestMapping("/api")
+public class XmlUploadController {
+
+    protected static final Logger logger = LogManager.getLogger();
+
+    @PostMapping("/xmluploadunsafe")
+    public ResponseEntity<Aanvraag> xmlUploadUnSafe(@RequestParam("file") MultipartFile file) {
+
+        Aanvraag aanvraag = parseAanvraagUnSafe( file);
+
+        return ResponseEntity.ok(aanvraag) ;
+
+    }
+
+    private Aanvraag parseAanvraagUnSafe(MultipartFile file) {
+
+        String SCHEMA_NS_URI = XMLConstants.W3C_XML_SCHEMA_NS_URI;
+        SchemaFactory sf = SchemaFactory.newInstance(SCHEMA_NS_URI);
+
+        Aanvraag aanvraag = null;
+
+        try(InputStream inputStream = file.getInputStream()){
+
+            JAXBContext jc = JAXBContext.newInstance("com.academy.training.generated");
+
+            XMLInputFactory xif = XMLInputFactory.newFactory();
+            xif.setProperty(XMLInputFactory.IS_SUPPORTING_EXTERNAL_ENTITIES, false);
+            xif.setProperty(XMLInputFactory.SUPPORT_DTD, true);
+
+            XMLStreamReader reader = xif.createXMLStreamReader(inputStream);
+
+            Unmarshaller u = jc.createUnmarshaller();
+
+            aanvraag = (Aanvraag)u.unmarshal(reader);
+
+        }catch (Exception e) {
+
+            logger.info("Oh oh...");
+        }
+
+        return aanvraag;
+
+    }
+
+}
